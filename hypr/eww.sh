4d1eb27676728c90fee809367cd63b3b1278d45f
Beechman <abeechey93@gmail.com>
2024-10-12 13:46:30 +0100

    making repo upto date for hyprland 0.41.2

--- hypr/eww.sh
+++ hypr/eww.sh
@@ -3,11 +3,4 @@
 eww daemon
 eww open clock
 eww open clock-lines
-eww open fetch
-eww open process
-eww open cpu-info
-eww open nv-info
-eww open hypr-splash
-eww open volume
-eww open workspace
-eww open internet
+eww open hypr-bar
---
+++ hypr/scripts/launch-bar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python ~/fabric/hypr-bar.py
+sleep .5
+python ~/fabric/hypr-clock.py
